14af64c27b3f66f8760a537e4695f67c7712dc7f
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-01 23:09:05 +0900

    Update ConnectDB.php

--- Util/ConnectDB.php
+++ Util/ConnectDB.php
@@ -4,8 +4,8 @@ class ConnectDB
     public static function connection()
     {
         $dsn = 'mysql:dbname=tb250205db;host=localhost';
-        $user = 'tb-250205';
-        $password = 'ZyHZaYsz5T';
+        $user = '*********';
+        $password = '*********';
         $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
         return $pdo;
     }
